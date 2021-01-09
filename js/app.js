53f48a8c8cf20c24e2e7b6b63f664b91918fa2c1
Abraham youkhanna <abrahamy3322@gmail.com>
2021-01-09 15:05:07 -0600

    added bar graph

--- js/app.js
+++ js/app.js
@@ -10,3 +10,12 @@ panel.append("h5").text(key+": "+value)
 
 
 })
+results = data.samples.filter(d=>d.id==sampleId)[0]
+var barData = [{
+y:results.otu_ids.slice(0,10).map(o=>"OTU "+o).reverse(),
+x:results.sample_values.slice(0,10).reverse(),
+text:results.otu_labels.slice(0,10).reverse(),
+type:"bar",
+orientation:"h"
+}
+]
